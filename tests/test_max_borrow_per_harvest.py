1e2a5323c772e985c8f96ee4360eb36bc7b5ccea
jmonteer <jmonteer@protonmail.com>
2021-05-04 18:38:51 +0200

    fix: details

--- tests/test_max_borrow_per_harvest.py
+++ tests/test_max_borrow_per_harvest.py
@@ -7,10 +7,10 @@ def test_max_borrow(vault, strategy, gov, wbtc, wbtc_whale, weth, weth_whale, yv
     wbtc.approve(vault, 2 ** 256 - 1, {"from": wbtc_whale})
     vault.deposit(10 * 1e8, {"from": wbtc_whale})
 
-    strategy.setMaxBorrowPerHarvest(0, {"from": gov})
+    strategy.setMaxTotalBorrowIT(0, {"from": gov})
     strategy.harvest({"from": gov})
-    assert strategy._balanceOfDebt() == 0
+    assert strategy.balanceOfDebt() == 0
 
-    strategy.setMaxBorrowPerHarvest(Wei("2 ether"), {"from": gov})
+    strategy.setMaxTotalBorrowIT(Wei("2 ether"), {"from": gov})
     strategy.harvest({"from": gov})
-    assert strategy._balanceOfDebt() == Wei("2 ether")
+    assert strategy.balanceOfDebt() == Wei("2 ether")

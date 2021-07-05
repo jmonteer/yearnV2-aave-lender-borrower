3df13ec7891bcc590075d05333a242bda3e04bb4
therealmonoloco <therealmonoloco@gmail.com>
2021-07-05 13:49:48 -0400

    test: fix huge debt increase test

--- tests/test_huge_debt_increase.py
+++ tests/test_huge_debt_increase.py
@@ -2,10 +2,10 @@ import pytest
 from brownie import chain, Wei, Contract
 
 
-def test_huge_debt(vault, strategy, gov, wbtc, wbtc_whale, weth, weth_whale, yvETH):
-    prev_balance = wbtc.balanceOf(wbtc_whale)
-    wbtc.approve(vault, 2 ** 256 - 1, {"from": wbtc_whale})
-    vault.deposit(10 * 1e8, {"from": wbtc_whale})
+def test_huge_debt(vault, strategy, gov, wmatic, wmatic_whale, dai, dai_whale, yvDAI):
+    prev_balance = wmatic.balanceOf(wmatic_whale)
+    wmatic.approve(vault, 2 ** 256 - 1, {"from": wmatic_whale})
+    vault.deposit(Wei("10 ether"), {"from": wmatic_whale})
     strategy.harvest({"from": gov})
     lp = get_lending_pool()
 
@@ -24,14 +24,14 @@ def test_huge_debt(vault, strategy, gov, wbtc, wbtc_whale, weth, weth_whale, yvE
     assert vault.strategies(strategy).dict()["totalLoss"] == 0
 
     vault.withdraw(
-        vault.balanceOf(wbtc_whale), wbtc_whale, 10_000, {"from": wbtc_whale}
+        vault.balanceOf(wmatic_whale), wmatic_whale, 10_000, {"from": wmatic_whale}
     )
 
-    print(f"diff {prev_balance-wbtc.balanceOf(wbtc_whale)}")
-    assert prev_balance - wbtc.balanceOf(wbtc_whale) > 0
+    print(f"diff {prev_balance-wmatic.balanceOf(wmatic_whale)}")
+    assert prev_balance - wmatic.balanceOf(wmatic_whale) > 0
 
 
 def get_lending_pool():
-    pd_provider = Contract("0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d")
+    pd_provider = Contract("0x7551b5D2763519d4e37e8B81929D336De671d46d")
     a_provider = Contract(pd_provider.ADDRESSES_PROVIDER())
     return Contract(a_provider.getLendingPool())

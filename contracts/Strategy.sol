3df6bfdcde3de3ab4403d6551a71b1dfe21c8cd5
therealmonoloco <therealmonoloco@gmail.com>
2021-07-05 13:41:15 -0400

    fix: wmatic address checksum

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -73,7 +73,7 @@ contract Strategy is BaseStrategy {
     IProtocolDataProvider internal constant protocolDataProvider =
         IProtocolDataProvider(0x7551b5D2763519d4e37e8B81929D336De671d46d);
 
-    address internal constant WMATIC = 0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270;
+    address internal constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     address internal constant WETH = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
 
     uint256 internal minThreshold;

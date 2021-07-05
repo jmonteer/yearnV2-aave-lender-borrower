c9f0bb35f80e870f23cb20e59a067939af5cc1e9
therealmonoloco <therealmonoloco@gmail.com>
2021-07-05 16:48:51 -0400

    test: add deploy test

---
+++ tests/integration/test_deploy.py
@@ -0,0 +1,21 @@
+from brownie import chain, Contract, Wei
+
+def test_deploy(vault, yvDAI, strategy, gov, wmatic, wmatic_whale, vddai):
+    wmatic.approve(vault, 2 ** 256 - 1, {"from": wmatic_whale})
+    vault.deposit(Wei("100000 ether"), {"from": wmatic_whale})
+
+    strategy.harvest({"from": gov})
+
+    # After first investment sleep for aproximately a year
+    chain.sleep(60 * 60 * 24 * 365)
+    chain.mine(1)
+
+    vault.revokeStrategy(strategy)
+    tx = strategy.harvest({"from": gov})
+    
+    data = vault.strategies(strategy).dict()
+    assert data["totalLoss"] == 0
+    assert data["totalDebt"] == 0
+    assert data["debtRatio"] == 0
+    assert yvDAI.balanceOf(strategy) == 0
+    

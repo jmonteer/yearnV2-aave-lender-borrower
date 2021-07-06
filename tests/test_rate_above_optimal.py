5d988130adb277b668f5d4225145a3311949f313
therealmonoloco <therealmonoloco@gmail.com>
2021-07-06 18:37:55 -0400

    test: wip rate above optimal

--- tests/test_rate_above_optimal.py
+++ tests/test_rate_above_optimal.py
@@ -1,24 +1,37 @@
 import pytest
-from brownie import chain, Wei, Contract, accounts, ZERO_ADDRESS
+from brownie import chain, Wei, Contract, accounts, interface
 
 
 def get_lp():
-    pd_provider = Contract("0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d")
+    pd_provider = Contract("0x7551b5D2763519d4e37e8B81929D336De671d46d")
     a_provider = Contract(pd_provider.ADDRESSES_PROVIDER())
     return Contract(a_provider.getLendingPool())
 
 
-def test_rate_above_optimal(
-    vault, strategy, gov, wbtc, wbtc_whale, vdweth, weth_whale, yvETH
+def test_rate_above(
+    vault, strategy, gov, wmatic, wmatic_whale, vddai,
 ):
-    wbtc.approve(vault, 2 ** 256 - 1, {"from": wbtc_whale})
-    vault.deposit(20 * 1e8, {"from": wbtc_whale})
+    wmatic.approve(vault, 2 ** 256 - 1, {"from": wmatic_whale})
+    vault.deposit(Wei("100 ether"), {"from": wmatic_whale})
 
-    # This will increase the rate to > 100%
+    # This will increase the rate to > 75%
     increase_interest()
 
+    # strategy.setStrategyParams(
+    #     strategy.targetLTVMultiplier(),
+    #     strategy.warningLTVMultiplier(),
+    #     0.75 * 1e27, # https://docs.aave.com/risk/liquidity-risk/borrow-interest-rate#dai-or-tusd
+    #     0,
+    #     strategy.maxTotalBorrowIT(),
+    #     strategy.isWantIncentivised(),
+    #     strategy.isInvestmentTokenIncentivised(),
+    #     strategy.leaveDebtBehind(),
+    #     strategy.maxLoss(),
+    #     {"from": strategy.strategist()},
+    # )
+
     strategy.harvest({"from": gov})
-    assert vdweth.balanceOf(strategy) == 0
+    assert vddai.balanceOf(strategy) == 0
 
     currentCost = (
         get_lp()
@@ -41,18 +54,22 @@ def test_rate_above_optimal(
     )
 
     strategy.harvest({"from": gov})
-    assert vdweth.balanceOf(strategy) > 0
+    assert vddai.balanceOf(strategy) > 0
 
 
 def increase_interest():
     lp = get_lp()
-    weth = Contract("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
-    aeth = Contract("0x030bA81f1c18d280636F32af80b9AAd02Cf0854e")
 
-    liquidity = weth.balanceOf(aeth)
-    to_move = liquidity - Wei("500 ether")
-    weth.transfer(ZERO_ADDRESS, to_move, {"from": aeth})
+    dai = Contract("0x8f3cf7ad23cd3cadbd9735aff958023239c6a063")
+    amdai = interface.IAToken("0x27f8d03b3a2196956ed754badc28d73be8830a6e")
+
+    whale = accounts.at("0x27f8d03b3a2196956ed754badc28d73be8830a6e", force=True)
+    dai.approve(lp, 2 ** 256 - 1, {"from": whale})
+    lp.deposit(dai, Wei("1 ether"), whale, 0, {"from": whale})
+
+    liquidity = dai.balanceOf(amdai)
+    to_move = liquidity - Wei("50_000 ether")
+    dai.transfer(dai, to_move, {"from": amdai})
 
     # Withdraw 1 ether to update the rates
-    whale = accounts.at("0x4deb3edd991cfd2fcdaa6dcfe5f1743f6e7d16a6", force=True)
-    lp.withdraw(weth, Wei("1 ether"), whale, {"from": whale})
+    lp.withdraw(dai, Wei("1 ether"), whale, {"from": whale})

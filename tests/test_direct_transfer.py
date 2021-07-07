e55286f45cf88628ad3af0b26f8fe00516224096
therealmonoloco <therealmonoloco@gmail.com>
2021-07-07 09:12:01 -0400

    test: check profit with direct transfer and actual gains

--- tests/test_direct_transfer.py
+++ tests/test_direct_transfer.py
@@ -1,4 +1,4 @@
-from brownie import Wei
+from brownie import Wei, chain
 
 
 def test_direct_transfer_increments_estimated_total_assets(
@@ -40,4 +40,33 @@ def test_deposit_should_not_increment_profits(
     vault.deposit(Wei("100 ether"), {"from": wmatic_whale})
     strategy.harvest()
     assert vault.strategies(strategy).dict()["totalGain"] == initialProfit
+
+
+def test_direct_transfer_with_actual_profits(
+    vault,
+    strategy,
+    wmatic,
+    wmatic_whale
+):
+    initialProfit = vault.strategies(strategy).dict()["totalGain"]
+    assert initialProfit == 0
+    
+    wmatic.approve(vault, 2 ** 256 - 1, {"from": wmatic_whale})
+    vault.deposit(Wei("100 ether"), {"from": wmatic_whale})
+    strategy.harvest()
+
+    # sleep for a day
+    chain.sleep(24 * 3600)
+    chain.mine(1)
+
+    # Receive a direct transfer
+    airdropAmount = Wei("100 ether")
+    wmatic.transfer(strategy, airdropAmount, {"from": wmatic_whale})
+    
+    # sleep for another day
+    chain.sleep(24 * 3600)
+    chain.mine(1)
+
+    strategy.harvest()
+    assert vault.strategies(strategy).dict()["totalGain"] > initialProfit + airdropAmount
 

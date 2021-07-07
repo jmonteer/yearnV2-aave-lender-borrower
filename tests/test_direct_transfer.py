61ce41ee916e07f4efeda4673d686dfec3573c8b
therealmonoloco <therealmonoloco@gmail.com>
2021-07-07 08:42:03 -0400

    test: direct transfers should count as profit

---
+++ tests/test_direct_transfer.py
@@ -0,0 +1,28 @@
+from brownie import Wei
+
+
+def test_direct_transfer_increments_estimated_total_assets(
+    strategy,
+    wmatic,
+    wmatic_whale,
+):
+    initial = strategy.estimatedTotalAssets()
+    amount = Wei("100 ether")
+    wmatic.transfer(strategy, amount, {"from": wmatic_whale})
+    assert strategy.estimatedTotalAssets() == initial + amount
+
+
+def test_direct_transfer_increments_profits(
+    vault,
+    strategy,
+    wmatic,
+    wmatic_whale
+):
+    initialProfit = vault.strategies(strategy).dict()["totalGain"]
+    assert initialProfit == 0
+
+    amount = Wei("100 ether")
+    wmatic.transfer(strategy, amount, {"from": wmatic_whale})
+    strategy.harvest()
+    assert vault.strategies(strategy).dict()["totalGain"] == initialProfit + amount
+

e234141d3ab5a401aa3b4d94269caaebe241c23d
therealmonoloco <therealmonoloco@gmail.com>
2021-07-09 14:44:44 -0400

    style: black formatting

--- scripts/matic_check_status.py
+++ scripts/matic_check_status.py
@@ -1,5 +1,6 @@
 from brownie import Contract
 
+
 def main():
     strat = Contract("0x2eF5D739E1052a5F18cBECb5C9E09254A8A11dB8")
     dai = Contract("0x8f3cf7ad23cd3cadbd9735aff958023239c6a063")
@@ -8,10 +9,9 @@ def main():
     lp = Contract(a_provider.getLendingPool())
 
     data = lp.getUserAccountData(strat).dict()
-    total_collateral = data['totalCollateralETH']
-    total_debt = data['totalDebtETH']
+    total_collateral = data["totalCollateralETH"]
+    total_debt = data["totalDebtETH"]
     print(f"LTV: {total_debt/total_collateral*100.0}")
 
     reserve_data = lp.getReserveData(dai).dict()
     print(f"Borrow rate: {reserve_data['currentVariableBorrowRate']/1e27*100.0}")
-
--- scripts/tender.py
+++ scripts/tender.py
@@ -1,9 +1,9 @@
 from brownie import Contract, accounts
 
+
 def main():
     keeper = accounts[0]
     strat = Contract("0x2eF5D739E1052a5F18cBECb5C9E09254A8A11dB8")
 
     if strat.tendTrigger(1):
         strat.tend({"from": keeper})
-

e95d7db169bb154485cd9cee8aaebdf82517ff26
therealmonoloco <therealmonoloco@gmail.com>
2021-07-16 09:59:21 -0400

    test: check profit with direct transfer and actual gains

--- tests/test_direct_transfer.py
+++ tests/test_direct_transfer.py
@@ -1,4 +1,4 @@
-from brownie import Wei
+from brownie import Wei, chain
 
 
 def test_direct_transfer_increments_estimated_total_assets(
@@ -30,3 +30,36 @@ def test_deposit_should_not_increment_profits(vault, strategy, token, token_whal
     vault.deposit(Wei("100 ether"), {"from": token_whale})
     strategy.harvest({"from": gov})
     assert vault.strategies(strategy).dict()["totalGain"] == initialProfit
+
+
+def test_direct_transfer_with_actual_profits(
+    vault, strategy, token, token_whale, borrow_token, borrow_whale, yvault, gov
+):
+    initialProfit = vault.strategies(strategy).dict()["totalGain"]
+    assert initialProfit == 0
+
+    token.approve(vault, 2 ** 256 - 1, {"from": token_whale})
+    vault.deposit(Wei("10 ether"), {"from": token_whale})
+    strategy.harvest({"from": gov})
+
+    # send some profit to yvault
+    borrow_token.transfer(
+        yvault, 20_000 * (10 ** borrow_token.decimals()), {"from": borrow_whale}
+    )
+
+    # sleep for a day
+    chain.sleep(24 * 3600)
+    chain.mine(1)
+
+    # receive a direct transfer
+    airdropAmount = Wei("10 ether")
+    token.transfer(strategy, airdropAmount, {"from": token_whale})
+
+    # sleep for another day
+    chain.sleep(24 * 3600)
+    chain.mine(1)
+
+    strategy.harvest({"from": gov})
+    assert (
+        vault.strategies(strategy).dict()["totalGain"] > initialProfit + airdropAmount
+    )

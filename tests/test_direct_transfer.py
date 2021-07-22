35c55791c3a13c1b07298300b65e3bcae6867cb4
therealmonoloco <therealmonoloco@gmail.com>
2021-07-22 07:46:50 -0400

    style: format using black 19.10b0

--- tests/test_direct_transfer.py
+++ tests/test_direct_transfer.py
@@ -2,9 +2,7 @@ from brownie import chain
 
 
 def test_direct_transfer_increments_estimated_total_assets(
-    strategy,
-    token,
-    token_whale,
+    strategy, token, token_whale,
 ):
     initial = strategy.estimatedTotalAssets()
     amount = 10 * (10 ** token.decimals())
